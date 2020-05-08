cba6ff87da4c3d586fc760a99cfea300ee587e2c
Dodo <s259044@studenti.polito.it>
2020-05-08 14:15:21 +0200

    First additions to Caltech class

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -15,11 +15,28 @@ def pil_loader(path):
 
 
 class Caltech(VisionDataset):
+    classes = {} # it contains the mappings class_name:numeric_label
+    excluded = ['BACKGROUND_Google']
+    class_num = 0 # counts how many classes we have
+
     def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
         self.split = split # This defines the split you are going to use
                            # (split files are called 'train.txt' and 'test.txt')
+
+        self.data = []    # it contains tuples (image, numeric_label)
+
+        with open(f"{split}.txt", 'r') as paths:
+            for line in paths.readlines():
+                class_name = line[:line.find('/')]
+                if (class_name not in Caltech.excluded) and (class_name not in Caltech.classes):
+                    Caltech.classes[class_name] = Caltech.class_num
+                    Caltech.class_num += 1
+                numeric_label = Caltech.classes[class_name]
+                img = pil_loader(line[:-1])
+
+                self.data.append( (img, numeric_label) )
 
         '''
         - Here you should implement the logic for reading the splits files and accessing elements
@@ -27,7 +44,7 @@ class Caltech(VisionDataset):
         - PyTorch Dataset classes use indexes to read elements
         - You should provide a way for the __getitem__ method to access the image-label pair
           through the index
-        - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
+        - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class)
         '''
 
     def __getitem__(self, index):
@@ -40,7 +57,7 @@ class Caltech(VisionDataset):
             tuple: (sample, target) where target is class_index of the target class.
         '''
 
-        image, label = ... # Provide a way to access image and label via index
+        image, label = self.data[index] # Provide a way to access image and label via index
                            # Image should be a PIL Image
                            # label can be int
 
@@ -55,5 +72,5 @@ class Caltech(VisionDataset):
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
-        length = ... # Provide a way to get the length (number of elements) of the dataset
+        length = len(self.data) # Provide a way to get the length (number of elements) of the dataset
         return length
